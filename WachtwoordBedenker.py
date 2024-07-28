934c9f660deb66e230be9a7a5df8bb2e5eb2e9b2
TheRealKiBa <164063047+TheRealKiBa@users.noreply.github.com>
2024-07-28 10:50:17 +0200

    Update WachtwoordBedenker.py

--- WachtwoordBedenker.py
+++ WachtwoordBedenker.py
@@ -1,12 +1,11 @@
 import random as rand
-wachtwoord = []
 
 def bedenk():
-    cijfers = int(input("Hoeveel cijfers moet het wachtwoord hebben?: "))
+    wachtwoord = []
+    cijfers = int(input("How many numbers in your password?: "))
     for i in range(cijfers):
         randomnum = rand.randint(1, 9)
         wachtwoord.append(randomnum)
-    tuple(wachtwoord)
     print("Dit is uw nieuwe wachtwoord: {}\n".format(wachtwoord))
     bedenk()
 
